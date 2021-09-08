ca9855416085896d58451ef3b383e8459bcd9036
EC2 Default User <ec2-user@ip-172-31-64-65.ec2.internal>
2021-09-08 22:55:19 +0000

    Chapter 1 Update

--- Chapter1/FirstNonRepeatableCharacter.java
+++ Chapter1/FirstNonRepeatableCharacter.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.util.stream.*;
+import java.util.function.*;
 
 class FirstNonRepeatableCharacter {
     
@@ -8,42 +9,74 @@ class FirstNonRepeatableCharacter {
         String a1 = "aabbccddee";   //null non-repeated
         String a2 = "abcde";        //a 1st nonrepeated
         String a3 = "abcdcbae";     //d 1st nonrepeated
-        String a4 = "abccbae";     //d 1st nonrepeated
+        String a4 = "abccbae";     //e 1st nonrepeated
+        String a5 = "💕💕🐱💖🎂🎂💻";
         
-        findFirstNonRepeatedCharacter(a1).ifPresent(System.out::println);
+        findFirstNonRepeatedCharacter(a1).ifPresentOrElse(System.out::println, () -> System.out.println(Character.MIN_VALUE));
         findFirstNonRepeatedCharacter(a2).ifPresent(System.out::println);
         findFirstNonRepeatedCharacter(a3).ifPresent(System.out::println);
         findFirstNonRepeatedCharacter(a4).ifPresent(System.out::println);
+        
+        findFirstNonRepeatedCodePoint(a5).ifPresent(System.out::println);
 
     }
     
+    // public static Optional<Character> findFirstNonRepeatedCharacter(String s) {
+        
+    //     Set<Character> repeatedSet = new HashSet<>();
+        
+    //     return IntStream.range(0, s.length())
+    //         .filter(i -> {
+    //             if (i == s.length() - 1) {
+    //                 return !repeatedSet.contains(s.charAt(i));
+    //             } else {
+    //                 if (s.substring(i + 1).indexOf(s.charAt(i)) < 0 &&
+    //                 !repeatedSet.contains(s.charAt(i))) {
+    //                     return true;
+    //                 } else {
+    //                     // System.out.println(repeatedSet);
+    //                     repeatedSet.add(s.charAt(i));
+    //                     return false;
+    //                 }
+    //             }
+            
+    //         })
+    //         .mapToObj(i -> {
+    //             // System.out.println(i);
+    //             // System.out.println(repeatedSet);
+    //             return s.charAt(i);
+    //         })
+    //         .findFirst();
+
+    // }
+    
     public static Optional<Character> findFirstNonRepeatedCharacter(String s) {
         
-        Set<Character> repeatedSet = new HashSet<>();
+        // Map<Character, Long> charMap = new LinkedHashMap<>();
         
-        return IntStream.range(0, s.length())
-            .filter(i -> {
-                if (i == s.length() - 1) {
-                    return !repeatedSet.contains(s.charAt(i));
-                } else {
-                    if (s.substring(i + 1).indexOf(s.charAt(i)) < 0 &&
-                    !repeatedSet.contains(s.charAt(i))) {
-                        return true;
-                    } else {
-                        // System.out.println(repeatedSet);
-                        repeatedSet.add(s.charAt(i));
-                        return false;
-                    }
-                }
-            
-            })
-            .mapToObj(i -> {
-                // System.out.println(i);
-                // System.out.println(repeatedSet);
-                return s.charAt(i);
-            })
-            .findFirst();
+        Map<Character, Long> charMap = s.chars()
+                        .mapToObj(i -> (char) i)
+                        .collect(Collectors.groupingBy(Function.identity(), () -> new LinkedHashMap<Character, Long>(), Collectors.counting()));
+        
+        return charMap.entrySet()
+                    .stream()
+                    .filter(entry -> entry.getValue() == 1)
+                    .map(entry -> entry.getKey())
+                    .findFirst();
+      
 
+    }
+    
+    public static Optional<String> findFirstNonRepeatedCodePoint(String s) {
+        
+        return s.codePoints()
+                    .mapToObj(i -> String.valueOf(Character.toChars(i)))
+                    .collect(Collectors.groupingBy(Function.identity(),() -> new LinkedHashMap<String, Long>(), Collectors.counting()))
+                    .entrySet()
+                    .stream()
+                    .filter(entry -> entry.getValue() == 1L)
+                    .map(entry -> entry.getKey())
+                    .findFirst();
     }
     
 }
